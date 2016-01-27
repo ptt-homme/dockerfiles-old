fe186c86d978aec50697614c18bffd6ccac48594
ptt-homme <tomcreaweb@gmail.com>
2016-01-27 07:57:45 +0100

    Update Laravel 5 starter

--- docker-laravel/docker-entrypoint.sh
+++ docker-laravel/docker-entrypoint.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 set -e
 
-exec apache2-foreground
+# Install Laravel.
+if ! [ -e package.json -a -d app ]; then
+	echo >&2 "Laravel not found in $(pwd) - copying now..."
+	if [ "$(ls -A)" ]; then
+		echo >&2 "WARNING: $(pwd) is not empty - press Ctrl+C now if this is an error!"
+		( set -x; ls -A; sleep 10 )
+	fi
+	tar cf - --one-file-system -C /usr/src/laravel . | tar xf -
+	echo >&2 "Complete! Laravel has been successfully copied to $(pwd)"
+fi
+
+# Set apache.
+sed -i 's,html,html/public,' /etc/apache2/apache2.conf
+
+exec "$@"
