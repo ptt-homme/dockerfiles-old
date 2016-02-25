303d0b0ac8839ae82becabf217f2a65a9d1d021c
ptt-homme <tomcreaweb@gmail.com>
2016-02-25 21:59:49 +0100

    Update Drupal 7 starter

--- docker-drupal7/docker-entrypoint.sh
+++ docker-drupal7/docker-entrypoint.sh
@@ -1,29 +1,20 @@
 #!/bin/bash
 set -e
 
-# Create base folder.
-PROJECT_PATH=/home/www-data/$PROJECT_URL
-mkdir -p $PROJECT_PATH
-cd $PROJECT_PATH
-
-if [ ! -d "www" ]; then
+# Install Drupal 7 from a existing repo or drush make.
+if ! [ -e index.php ] && ! [ -d sites ]; then
 
 	if [ -n "$PROJECT_GIT_REPO" ]; then
-		git clone $PROJECT_GIT_REPO www
+		git clone "$PROJECT_GIT_REPO" .
 	else
-		/root/.composer/vendor/bin/drush make /tmp/makefiles/drupal.make www -y
+		~/.composer/vendor/bin/drush make /tmp/makefiles/drupal.make www -y
 		cd www/sites/default
 		mkdir files && chmod 777 files
 		cp -p default.settings.php settings.php
 	fi
 
-	cd $PROJECT_PATH
-	chown -R www-data:www-data www
-
-	rm -rf /var/www/html
+	chown -R www-data:www-data .
 
 fi
 
-ln -s "${PROJECT_PATH}/www" /var/www/html
-
-exec apache2-foreground
+exec "$@"
