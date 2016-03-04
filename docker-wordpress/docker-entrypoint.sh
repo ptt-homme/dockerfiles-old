880280a3e68b59ce96a65c767e3035838823588c
ptt-homme <tomcreaweb@gmail.com>
2016-03-04 08:13:31 +0100

    Update WordPress starter

--- docker-wordpress/docker-entrypoint.sh
+++ docker-wordpress/docker-entrypoint.sh
@@ -1,25 +1,17 @@
 #!/bin/bash
 set -e
 
-# Create base folder.
-PROJECT_PATH=/home/www-data/$PROJECT_URL
-mkdir -p $PROJECT_PATH
-cd $PROJECT_PATH
-
-if [ ! -d "www" ]; then
+# Install WordPress.
+if ! [ -e index.php ] && ! [ -d wp-content ]; then
 
 	if [ -n "$PROJECT_GIT_REPO" ]; then
-		git clone $PROJECT_GIT_REPO www
+		git clone $PROJECT_GIT_REPO .
 	else
-		git clone https://github.com/ptt-homme/WordPress_starter.git www
+		git clone https://github.com/ptt-homme/WordPress_starter.git .
 	fi
 
-	chown -R www-data:www-data www
-
-	rm -rf /var/www/html
+	chown -R www-data:www-data .
 
 fi
 
-ln -s "${PROJECT_PATH}/www" /var/www/html
-
-exec apache2-foreground
+exec "$@"
